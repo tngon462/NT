7ec1088c6ba24854ef7e106e71eb48ac66536e92
tngon462 <tngon462@gmail.com>
2025-12-13 17:01:44 +0900

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// js/firebase-config.js
+window.firebaseConfig = {
+  apiKey: "AIzaSyAKLCHWVS3V24J6G2lAzxV55zKvBw5lm58",
+  authDomain: "thiepmen-nhatro.firebaseapp.com",
+  projectId: "thiepmen-nhatro",
+  storageBucket: "thiepmen-nhatro.firebasestorage.app",
+  messagingSenderId: "378377382710",
+  appId: "1:378377382710:web:76e8501e11d648df11a197"
+};
